53905d882466b5dd99c42f610900e58c558e8ce0
MoonJaeGyeong <bblbblan69@gmail.com>
2025-10-03 20:57:32 +0900

    build : 프로젝트 초기 설정 및 의존성 추가

---
+++ JjimKong_Backend/src/main/java/com/jjimkong_backend/JjimKongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jjimkong_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjimKongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjimKongBackendApplication.class, args);
+    }
+
+}
---
+++ JjimKong_Backend/src/test/java/com/jjimkong_backend/JjimKongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjimkong_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjimKongBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
